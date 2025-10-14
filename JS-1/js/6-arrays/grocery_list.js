016f6bc8f5b0069c0ddadd0b7782f29cedf92bc1
“ZohaBahman” <zoha.bahman@gmail.com>
2025-10-14 18:36:47 +0200

    array lists added

---
+++ JS-1/js/6-arrays/grocery_list.js
@@ -0,0 +1,21 @@
+let groceriesList = [
+  {
+    name: "Milk",
+    amount: 2,
+    bought: false
+  },
+  {
+    name: "Bread",
+    amount: 1,
+    bought: false
+  },
+  {
+    name: "Eggs",
+    amount: 12,
+    bought: false
+  }
+];
+console.log(groceriesList);
+
+
+
---
+++ JS-1/js/6-arrays/receipt.js
@@ -0,0 +1,29 @@
+let shoppingReceipt = [
+    {
+        name: "Milk",
+        amount: 2,
+        pricePerItem: 1.99
+    },
+    {
+        name: "Bread",
+        amount: 1, 
+        pricePerItem: 2.50
+    },
+    {
+        name: "Apples",
+        amount: 6,
+        pricePerItem: 0.25
+    },
+    {
+        name: "Chicken",
+        amount: 1,
+        pricePerItem: 8.75
+    },
+    {
+        name: "Rice",
+        amount: 2,
+        pricePerItem: 3.20
+    }
+];
+
+console.log(shoppingReceipt);
