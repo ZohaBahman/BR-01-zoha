94bb93cd8c409bf370c1ab9f20ca0cd8991a98fc
“ZohaBahman” <zoha.bahman@gmail.com>
2025-12-05 18:48:07 +0100

    project 2

--- JS-1/js/Project_2/JS/main.js
+++ JS-1/js/Project_2/JS/main.js
@@ -6,4 +6,31 @@ $(document).ready(function(){
     autoplay: true,
     infinite: true,
 });
+
+
+
+$('.scroll_down_btn').on('click', function(){
+  $('html, body').animate({
+    scrollTop: $('#what_we_do').offset().top
+  }, 800);
 });
+
+});
+
+
+
+$(document).ready(function(){
+  $('.news_slider').slick({
+    dots: true,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+
+    prevArrow: $('.news_arrow_prev'),
+    nextArrow: $('.news_arrow_next'),
+
+  });
+});
+
+
